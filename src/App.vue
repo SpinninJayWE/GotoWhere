927370213fbf404685e843566f5921fdcdf3bb1e
Spinnin <1315025826@qq.com>
2019-05-31 21:38:59 +0800

    fist commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h2>这是App首页</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+ h2{
+   font-size: 18px;
+   padding: 8px;
+   text-align: center;
+ }
+
+</style>
